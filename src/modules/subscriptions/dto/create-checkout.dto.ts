26f1953a962f4ba3e69a511949597951fa6c878c
Suraj-Upadhyay <usuraj35@gmail.com>
2026-04-16 12:53:51 +0530

    feat(project): add subscription module with its dto

---
+++ src/modules/subscriptions/dto/create-checkout.dto.ts
@@ -0,0 +1,15 @@
+import { IsEnum, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum PlanType {
+  BASIC = 'basic',
+  STANDARD = 'standard',
+  PREMIUM = 'premium',
+}
+
+export class CreateCheckoutDto {
+  @ApiProperty({ enum: PlanType, example: PlanType.STANDARD })
+  @IsEnum(PlanType)
+  @IsNotEmpty()
+  planId: PlanType;
+}
